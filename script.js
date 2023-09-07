6228f95e7e426e5314570c36eec1e12151561dd2
cpur0011 <cpur0011@student.monash.edu>
2023-09-07 15:06:37 +0000

    Update about.html, services.html, script.js, 2101website.html, LeanTeamPhotograph.png

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Document is fully loaded and parsed.");
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutLink = document.getElementById('aboutLink');
+
+    aboutLink.addEventListener('click', function(event) {
+        event.preventDefault();  // Prevent the default behavior of the link
+        window.location.href = 'about.html';  // Redirect to the about.html page
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const aboutLink = document.getElementById('servicesLink');
+
+    aboutLink.addEventListener('click', function(event) {
+        event.preventDefault();  // Prevent the default behavior of the link
+        window.location.href = 'services.html';  // Redirect to the about.html page
+    });
+});
 
