ec12fc1047933d183243d217bdaea9c0f5588266
Tom Macauley <tmac0014@student.monash.edu>
2023-09-09 16:06:28 +1000

    Added login from gmail and adjusted ui

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ document.querySelector("#show-login").addEventListener("click",function(){
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
 });
+
+
+document.querySelector(".popup .signup-link").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
