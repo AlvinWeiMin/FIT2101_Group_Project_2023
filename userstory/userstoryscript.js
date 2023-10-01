f52fc4b74331ea975994993e1f6074000510fbbc
Tom Macauley <tmac0014@student.monash.edu>
2023-10-02 00:13:38 +1100

    Added way to store user stories into database and then display them realtime in backlog

--- userstory/userstoryscript.js
+++ userstory/userstoryscript.js
@@ -1,27 +1,128 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const userStoryForm = document.getElementById('userStoryForm');
-    const backlogList = document.getElementById('backlogList');
 
-    userStoryForm.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      
-      const formData = new FormData(userStoryForm);
-      const response = await fetch('/api/user-stories', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Object.fromEntries(formData)),
-      });
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  import { getDatabase, ref, set, get, child, push , onValue} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
-      if (response.ok) {
-        const newUserStory = await response.json();
-        const listItem = document.createElement('li');
-        listItem.textContent = `${newUserStory.title} (Epic: ${newUserStory.epic}, Estimate: ${newUserStory.estimate})`;
-        backlogList.appendChild(listItem);
-        userStoryForm.reset();
-      } else {
-        alert('Error creating user story.');
-      }
-    });
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1JWDoe41ljxU6Wx2K3xDlzkTmQ_pHWyc",
+  authDomain: "fit2101-a1-lean-3469e.firebaseapp.com",
+  databaseURL: "https://fit2101-a1-lean-3469e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fit2101-a1-lean-3469e",
+  storageBucket: "fit2101-a1-lean-3469e.appspot.com",
+  messagingSenderId: "624522420359",
+  appId: "1:624522420359:web:5912dbfcb057d58a6c1822"
+};
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getDatabase(app);
+  
+const userStoryForm = document.getElementById('userStoryForm');
+
+
+// document.getElementById("submituserstory").addEventListener('click' , function(e){
+//   e.preventDefault();
+//   set(ref(db , 'userstories/' ),
+//   {
+//     title : document.getElementById('title').value,
+//     desc : document.getElementById('description').value,
+//     epic : document.getElementById('epic').value,
+//     estimate : document.getElementById('estimate').value,
+//     assignee : document.getElementById('assigned-to').value
+
+
+//   })
+
+// })
+document.addEventListener("DOMContentLoaded", function() {
+  const userStoryForm = document.getElementById('userStoryForm');
+
+  userStoryForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    createStory(
+      document.getElementById('title').value,
+      document.getElementById('description').value,
+      document.getElementById('epic').value,
+      document.getElementById('estimate').value,
+      document.getElementById('assigned-to').value
+    );
   });
+
+  function createStory(title, description, epic, estimate, assignee) {
+    // Your code to save the user story to Firebase here
+
+    const userStoryRef =  ref(db, 'userstories/');
+    const newUserStory = push(userStoryRef);
+
+
+    set(newUserStory, {
+      title: title,
+      desc: description,
+      epic: epic,
+      estimate: estimate,
+      assignee: assignee
+    });
+    
+    alert("ADDED STORY");
+  }
+});
+
+
+
+
+var tbody = document.getElementById('backlogbody');
+
+
+
+function AddStoryToTable(title, desc, epic, estimate , assignee){
+  
+
+  let trow = document.createElement('tr');
+
+  let td1 = document.createElement('td');
+  let td2 = document.createElement('td');
+  let td3 = document.createElement('td');
+  let td4 = document.createElement('td');
+  let td5 = document.createElement('td');
+
+  td1.innerHTML = title;
+  td2.innerHTML = desc;
+  td3.innerHTML = epic;
+  td4.innerHTML = estimate;
+  td5.innerHTML = assignee;
+
+  trow.appendChild(td1); trow.appendChild(td2); trow.appendChild(td3); trow.appendChild(td4); trow.appendChild(td5);
+  tbody.appendChild(trow)
+}
+
+function AddAllItemsToTable(story){
+  
+  //tbody.innerHTML = "";
+  story.forEach(element => {
+    AddStoryToTable(element.title , element.desc , element.epic , element.estimate , element.assignee);
+  })
+}
+
+function SelectAllData(){
+
+  const dbRef = ref(db ,  "userstories");
+
+  onValue(dbRef, (snapshot) => {
+
+    var stories = [];
+
+    snapshot.forEach(childSnapshot => {
+
+      stories.push(childSnapshot.val());
+    
+    });
+
+    AddAllItemsToTable(stories);
+  })
+
+}
+window.onload = SelectAllData;
