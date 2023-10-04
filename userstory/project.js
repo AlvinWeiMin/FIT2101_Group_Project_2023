2607d3746f73bd1546e6c127e159ba636fac4e59
Shan <slow0020@student.monash.edu>
2023-10-04 16:33:07 +1100

    added data storage for firebase

--- userstory/project.js
+++ userstory/project.js
@@ -1,18 +1,60 @@
-const projectForm = document.getElementById("projectForm");
-const projectName = document.getElementById("projectName");
-const projectList = document.getElementById("projectList");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9LgYBVCVqnQt3oGrXBwLDMY3GFMKZC7I",
+  authDomain: "fit2101-482ae.firebaseapp.com",
+  projectId: "fit2101-482ae",
+  storageBucket: "fit2101-482ae.appspot.com",
+  messagingSenderId: "386692113129",
+  appId: "1:386692113129:web:94119aa1b4161851d6c678",
+  measurementId: "G-VR8V7N1RBT"
+};
 
-const addProject = (projectName) => {
-    const projectLst = document.createElement("li");
-    projectLst.textContent = projectName;
-    projectList.appendChild(projectLst);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const projectForm = document.getElementById('projectForm');
+var storyID = 0
+
+// CREATING A PROJECT
+document.addEventListener("DOMContentLoaded", function() {
+  const projectForm = document.getElementById('projectForm');
+
+  projectForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    createProject(
+      document.getElementById('projectName').value,
+      document.getElementById('projectDescription').value,
+      document.getElementById('productOwner').value,
+      document.getElementById('scrumMaster').value,
+      document.getElementById('projectStartDate').value,
+      document.getElementById('projectEndDate').value
+    );
+  });
+
+  function createProject(projectName, projectDesc, prodOwner, scrumMaster, projectStart, projectEnd) {
+    // Your code to save the user story to Firebase here
+
+    const projectRef =  ref(database, 'projects/' + projectname);
+
+    set(projectRef, {
+      projectname: projectName,
+      projectdesc: projectDesc,
+      productowner: prodOwner,
+      scrummaster: scrumMaster,
+      projectstart: projectStart,
+      projectend: projectEnd
+    });
+
+    alert("ADDED PROJECT");
 }
-
-projectForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const projectName = projectNameInput.value;
-
-    addProject(projectName);
-    projectNameInput.value = "";
 });
+
